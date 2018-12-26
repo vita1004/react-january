145efccbc32f843f29dcf73a1f205c55744aa277
vita1004 <josephj@grantep.com>
2018-12-26 16:54:30 -0500

    Initial commit

---
+++ src/Api.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class Api extends Component {
+    state = {
+        data: [],
+        // description: [],
+        isLoading: false,
+        error: null,
+    };
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+        const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+        const url2 = "http://testgis3.grantep.com/items/getbiitm01";
+        const API = 'https://hn.algolia.com/api/v1/search?query=';
+        const DEFAULT_QUERY = 'redux';
+        const url3 = 'http://api.icndb.com/jokes';
+        
+        fetch(url2)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then(data => this.setState({ data: data, isLoading: false }))
+            .catch(error => this.setState({ error, isLoading: false }));
+            
+    }
+
+    render() {
+        const { data , isLoading, error } = this.state;
+        // const result = data.map((entry, index) => {
+        //     return <li key={data.objectID}></li>;
+        // });
+        if (error){
+            return <p>{error.message}</p>;
+        }
+        if (isLoading){
+            return <p>Loading ...</p>;
+        }
+        return (
+            <div>
+                <tr>
+                    <th>Item No</th>
+                    <th>Description</th>
+                </tr>
+                    {this.state.data.map(data => 
+                        <div key={data}> 
+                            <tr>
+                                <td>{data.itemNo}</td>
+                                <td>{data.description}</td> 
+                            </tr>
+                        </div>)}
+            </div>
+        );
+    }
+}
+
+export default Api;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './Api';
 import * as serviceWorker from './serviceWorker';
+// import App from './Api';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
