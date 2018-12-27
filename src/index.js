ea1fd88a556500939d81fe05579e4c51e7c0abfc
vita1004 <josephj@grantep.com>
2018-12-27 10:38:50 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App from './Api';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import App from './Api';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
